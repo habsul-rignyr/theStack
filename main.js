21b5e6b36212ecac0a1f9575962616ed1b786f1b
Josh <joshamy@gmail.com>
2021-11-02 12:37:53 +0800

    Improve heading and body columns

--- main.js
+++
@@ -1,9 +0,0 @@
-let lastclick;
-
-function frameIt(x) {
-  if (x != lastclick) {
-    let epframe = document.getElementById("episodeFrame");
-    epframe.src = `episodePages/${x}.html`;
-    lastclick = x;
-  }
-}
