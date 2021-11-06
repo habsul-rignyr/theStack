ebba31fb79027f7856ace45e4467486c4cc0866a
Josh <joshamy@gmail.com>
2021-11-06 21:04:36 +0800

    Add RSS button

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ function copyFunction(x) {
     var copyText = "0xB5F99027D52aacAaC62CEEA57fd44DDfefc70674";
   } else if (x === "btcButton") {
     var copyText = "bc1qchy43jrsa4yvv0am8s6hkm6q2sw46gjw5hachy";
+  } else if (x === "stackRSS") {
+    var copyText =
+      "https://feeds.soundcloud.com/users/soundcloud:users:900166441/sounds.rss";
   }
 
   /* Copy the text inside the text field */
